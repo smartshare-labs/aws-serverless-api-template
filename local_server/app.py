de3f433132e3a2dbedb617e17438ca8c0644a64f
luke <luke@strongarmtech.com>
2019-06-04 14:53:21 -0400

    set up flask local server for a single endpoint

---
+++ local_server/app.py
@@ -0,0 +1,25 @@
+import json
+from flask import Flask
+from flask import jsonify
+
+# Import your lambda function handlers here
+from functions.hello import handler as hello
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def _hello():
+    test_event = {
+    }
+
+    test_context = {
+
+    }
+
+    response = hello.hello(test_event, test_context)
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
