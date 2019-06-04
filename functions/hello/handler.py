f20a893e4874aa36067ec81cb785451e3ebb0392
luke <luke@strongarmtech.com>
2019-06-04 13:51:11 -0400

    initial commit

---
+++ functions/hello/handler.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+
+def hello(event, context):
+    body = {
+        "message": "Hello world",
+        "input": event,
+        "secret":  os.getenv('SOME_VAR', 'nada')
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
